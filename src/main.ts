b9d9387d37f1c439da90bf406e4325b96a5ac28f
wisnia74 <jakub.wisniowski74@gmail.com>
2025-04-09 22:03:48 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TypeORMExceptionFilter } from './typeorm-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new TypeORMExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('star-wars-app')
+    .setDescription('star-wars-app API description')
+    .setVersion('1.0')
+    .addTag('star-wars')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(parseInt(process.env.PORT!, 10));
 }
